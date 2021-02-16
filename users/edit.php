8265e2fc6f66360d7667cbf3733a1c5892d01d45
Maxim Grin <maks.grin.2016@gmail.com>
2021-02-16 13:30:32 +0300

    2

--- users/edit.php
+++ users/edit.php
@@ -1,73 +1,126 @@
+<?php
+require_once "..\init.php";
+
+$user = new User;
+if (!$user->isLoggedIn()) {
+  if (!$user->hasPermissions('admin')) {
+    Redirect::to('..\index.php');
+  }
+}
+
+$edit_id = $_GET['id'];
+
+if (Input::exists()) {
+  if (Token::check(Input::get('token'))) {
+    $validate = new Validate;
+
+    $validation = $validate->check($_POST, [
+      'username' => ['min' => 3, 'max' => 15],
+      'status' => ['max' => 500]
+    ]);
+
+    if ($validation->passed()) {
+      $user->update([
+        'username' => Input::get('username'),
+        'profile_status' => Input::get('status')
+      ], $edit_id);
+      Session::flash('success', 'Профиль обновлен');
+    } else {
+      $danger = '';
+      foreach ($validation->errors() as $error) {
+        $danger .= '<li>' . $error . '</li> ' . ' <br>';
+      }
+      $danger = rtrim($danger, "<br>");
+      Session::flash('danger', $danger);
+    }
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Profile</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-  
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">User Management</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Главная</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Управление пользователями</a>
-            </li>
-          </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="..\index.php">User Management</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <li class="nav-item">
-                <a href="profile.html" class="nav-link">Профиль</a>
-              </li>
-              <a href="#" class="nav-link">Выйти</a>
-            </li>
-          </ul>
-        </div>
-    </nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="..\index.php">Главная</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Управление пользователями</a>
+        </li>
+      </ul>
 
-   <div class="container">
-     <div class="row">
-       <div class="col-md-8">
-         <h1>Профиль пользователя - Рахим</h1>
-         <div class="alert alert-success">Профиль обновлен</div>
-         
-         <div class="alert alert-danger">
-           <ul>
-             <li>Ошибка валидации</li>
-           </ul>
-         </div>
-         <form action="" class="form">
-           <div class="form-group">
-             <label for="username">Имя</label>
-             <input type="text" id="username" class="form-control" value="Рахим">
-           </div>
-           <div class="form-group">
-             <label for="status">Статус</label>
-             <input type="text" id="status" class="form-control" value="Разрабатываю новые проекты)">
-           </div>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+        <li class="nav-item">
+          <a href="..\profile.php" class="nav-link">Профиль</a>
+        </li>
+        <a href="..\logout.php" class="nav-link">Выйти</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <?php 
+    $edit_user = Database::getInstance()->get('test', ['id', '=', $_GET['id']])->first(); 
+  ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h1>Профиль пользователя - <?php echo $edit_user->username; ?></h1>
 
-           <div class="form-group">
-             <button class="btn btn-warning">Обновить</button>
-           </div>
-         </form>
+        <?php if (Session::exists('danger')) : ?>
+          <div class="alert alert-danger">
+            <ul>
+              <?php echo Session::flash('danger'); ?>
+            </ul>
+          </div>
+        <?php endif; ?>
+
+        <?php if (Session::exists('success')) : ?>
+          <div class="alert alert-success">
+            <?php echo Session::flash('success'); ?>
+          </div>
+        <?php endif; ?>
+
+        <form action="edit.php?id=<?php echo $_GET['id']; ?>" class="form" method="POST">
+          <div class="form-group">
+            <label for="username">Имя</label>
+            <input type="text" id="username" name="username" class="form-control" value="<?php echo $edit_user->username; ?>">
+          </div>
+          <div class="form-group">
+            <label for="status">Статус</label>
+            <input type="text" id="status" name="status" class="form-control" value="<?php echo $edit_user->profile_status; ?>">
+          </div>
+
+          <div class="form-group">
+            <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+          </div>
+
+          <div class="form-group">
+            <button class="btn btn-warning">Обновить</button>
+          </div>
+        </form>
 
 
-       </div>
-     </div>
-   </div>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
--- users/index.php
+++ users/index.php
@@ -1,9 +1,21 @@
+<?php 
+require_once "..\init.php";
+
+$admin = new User;
+if (!$admin->isLoggedIn()) {
+  if (!$admin->hasPermissions('admin')) {
+    Redirect::to('..\index.php');
+  }
+}
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Users</title>
+
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <!-- Bootstrap core CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -12,7 +24,7 @@
 
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">User Management</a>
+      <a class="navbar-brand" href="..\index.php">User Management</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,24 +32,24 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Главная</a>
+            <a class="nav-link" href="..\index.php">Главная</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Управление пользователями</a>
+            <a class="nav-link" href="index.php">Управление пользователями</a>
           </li>
         </ul>
 
         <ul class="navbar-nav">
           <li class="nav-item">
             <li class="nav-item">
-              <a href="profile.html" class="nav-link">Профиль</a>
+              <a href="..\profile.php" class="nav-link">Профиль</a>
             </li>
-            <a href="#" class="nav-link">Выйти</a>
+            <a href="..\logout.php" class="nav-link">Выйти</a>
           </li>
         </ul>
       </div>
-  </nav>
-
+    </nav>
+    <?php $users = Database::getInstance()->get('test', ['id', '>', 0])->results(); ?>
     <div class="container">
       <div class="col-md-12">
         <h1>Пользователи</h1>
@@ -52,44 +64,29 @@
           </thead>
 
           <tbody>
+          <?php foreach ($users as $user): ?>
+          <?php if ($user->id == $admin->data()->id) {continue;} ?>
             <tr>
-              <td>1</td>
-              <td>Rahim</td>
-              <td>rahim@marlindev.ru</td>
+              <td><?php echo $user->id; ?></td>
+              <td><?php echo $user->username; ?></td>
+              <td><?php echo $user->email; ?></td>
               <td>
-              	<a href="#" class="btn btn-success">Назначить администратором</a>
-                <a href="#" class="btn btn-info">Посмотреть</a>
-                <a href="#" class="btn btn-warning">Редактировать</a>
-                <a href="#" class="btn btn-danger" onclick="return confirm('Вы уверены?');">Удалить</a>
+              	<a href="changepermission.php?id=<?php echo $user->id; ?>" 
+                class="<?php echo ($user->group_id == 1) ? "btn btn-success" : "btn btn-danger";?>">
+                  <?php 
+                  echo ($user->group_id == 1) ? "Назначить администратором" : "Разжаловать";
+                  ?>
+                </a>
+                <a href="../user_profile.php?id=<?php echo $user->id; ?>" class="btn btn-info">Посмотреть</a>
+                <a href="edit.php?id=<?php echo $user->id; ?>" class="btn btn-warning">Редактировать</a>
+                <a href="delete.php?id=<?php echo $user->id; ?>" class="btn btn-danger" onclick="return confirm('Вы уверены?');">Удалить</a>
               </td>
             </tr>
+          <?php endforeach; ?>
 
-            <tr>
-              <td>2</td>
-              <td>John</td>
-              <td>john@marlindev.ru</td>
-              <td>
-              	<a href="#" class="btn btn-danger">Разжаловать</a>
-                <a href="#" class="btn btn-info">Посмотреть</a>
-                <a href="#" class="btn btn-warning">Редактировать</a>
-                <a href="#" class="btn btn-danger" onclick="return confirm('Вы уверены?');">Удалить</a>
-              </td>
-            </tr>
-
-            <tr>
-              <td>3</td>
-              <td>Jane</td>
-              <td>jane@marlindev.ru</td>
-              <td>
-              	<a href="#" class="btn btn-success">Назначить администратором</a>
-                <a href="#" class="btn btn-info">Посмотреть</a>
-                <a href="#" class="btn btn-warning">Редактировать</a>
-                <a href="#" class="btn btn-danger" onclick="return confirm('Вы уверены?');">Удалить</a>
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
-    </div>
+    </div>  
   </body>
 </html>
